ad97558d0b01aa08733497bddde4d87fcb6da147
Leo Wallentin <leo_wallentin@hotmail.com>
2012-02-02 19:15:48 +0100

    0.2.1

---
+++ SemanticExtraSpecialProperties.i18n.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Internationalisation file for extension SemanticExtraSpecialProperties
+ *
+ * @file SemanticExtraSpecialProperties.i18n.php
+ * @ingroup Extensions
+ */
+ 
+$messages = array();
+ 
+/** English
+ * @author Rotsee
+ */
+$messages['en'] = array(
+        'sesp-desc' => 'Adds some extra special properties to all pages',
+        'sesp-property-author' => 'Page author',
+        'sesp-property-first-author' => 'Page creator',
+        'sesp-property-revision-id' => 'Revision ID',
+        'sesp-property-view-count' => 'Number of page views',
+        'sesp-property-subpages' => 'Subpage',
+        'sesp-property-revisions' => 'Number of revisions',
+        'sesp-property-talk-revisions' => 'Number of talk page revisions',
+);
+ 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+        'sesp-desc' => 'Stellt zusätzliche Spezialattribute zur Verfügung',
+        'sesp-property-author' => 'Seitenbearbeiter',
+        'sesp-property-first-author' => 'Seitenersteller',
+        'sesp-property-revision-id' => 'Bearbeitungskennung',
+        'sesp-property-view-count' => 'Anzahl der Seitenaufrufe',
+        'sesp-property-subpages' => 'Unterseite',
+        'sesp-property-revisions' => 'Anzahl der Bearbeitungen',
+        'sesp-property-talk-revisions' => 'Anzahl der Diskussionsbearbeitungen',
+);
+ 
+/** Swedish (Svenska)
+ * @author Rotsee
+ */
+$messages['sv'] = array(
+        'sesp-desc' => 'Extra specialegenskaper på alla sidor',
+        'sesp-property-author' => 'Sidförfattare',
+        'sesp-property-first-author' => 'Sidskapare',
+        'sesp-property-revision-id' => 'Versions-id',
+        'sesp-property-view-count' => 'Antal sidvisningar',
+        'sesp-property-subpages' => 'Undersida',
+        'sesp-property-revisions' => 'Antal sidredigeringar',
+        'sesp-property-talk-revisions' => 'Antal redigeringar på diskussionssidan',
+);
