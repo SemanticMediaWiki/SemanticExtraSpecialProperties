1fa0b1b8fdff3b1bf6e260482d04050a67df08be
jeroendedauw <jeroendedauw@gmail.com>
2012-11-07 00:04:26 +0100

    Fixed indent
    Change-Id: Ic3b8a3cf89f8407f49123bd60a7975186d10eda6

--- SemanticExtraSpecialProperties.i18n.php
+++ SemanticExtraSpecialProperties.i18n.php
@@ -12,17 +12,17 @@ $messages = array();
  * @author Rotsee, MWJames
  */
 $messages['en'] = array(
-        'sesp-desc' => 'Adds some extra special properties to all pages',
-        'sesp-property-author' => 'Page author',
-        'sesp-property-first-author' => 'Page creator',
-        'sesp-property-revision-id' => 'Revision ID',
-        'sesp-property-view-count' => 'Number of page views',
-        'sesp-property-subpages' => 'Subpage',
-        'sesp-property-revisions' => 'Number of revisions',
-        'sesp-property-talk-revisions' => 'Number of talk page revisions',
-        'sesp-property-mimetype' => 'MIME type',
-        'sesp-property-mediatype' => 'Media type',
-        'sesp-property-shorturl' => 'Short URL',
+	'sesp-desc' => 'Adds some extra special properties to all pages',
+	'sesp-property-author' => 'Page author',
+	'sesp-property-first-author' => 'Page creator',
+	'sesp-property-revision-id' => 'Revision ID',
+	'sesp-property-view-count' => 'Number of page views',
+	'sesp-property-subpages' => 'Subpage',
+	'sesp-property-revisions' => 'Number of revisions',
+	'sesp-property-talk-revisions' => 'Number of talk page revisions',
+	'sesp-property-mimetype' => 'MIME type',
+	'sesp-property-mediatype' => 'Media type',
+	'sesp-property-shorturl' => 'Short URL',
 	'sesp-property-user-registration-date' => 'User registration date',
 );
  
@@ -30,34 +30,34 @@ $messages['en'] = array(
  * @author Kghbln, Rotsee
  */
 $messages['de'] = array(
-        'sesp-desc' => 'Stellt zusätzliche Spezialattribute zur Verfügung',
-        'sesp-property-author' => 'Seitenbearbeiter',
-        'sesp-property-first-author' => 'Seitenersteller',
-        'sesp-property-revision-id' => 'Bearbeitungskennung',
-        'sesp-property-view-count' => 'Anzahl der Seitenaufrufe',
-        'sesp-property-subpages' => 'Unterseite',
-        'sesp-property-revisions' => 'Anzahl der Bearbeitungen',
-        'sesp-property-talk-revisions' => 'Anzahl der Diskussionsbearbeitungen',
-        'sesp-property-mimetype' => 'MIME-Type',
-        'sesp-property-mediatype' => 'Medientyp',
-        'sesp-property-shorturl' => 'Kurz-URL',
+	'sesp-desc' => 'Stellt zusätzliche Spezialattribute zur Verfügung',
+	'sesp-property-author' => 'Seitenbearbeiter',
+	'sesp-property-first-author' => 'Seitenersteller',
+	'sesp-property-revision-id' => 'Bearbeitungskennung',
+	'sesp-property-view-count' => 'Anzahl der Seitenaufrufe',
+	'sesp-property-subpages' => 'Unterseite',
+	'sesp-property-revisions' => 'Anzahl der Bearbeitungen',
+	'sesp-property-talk-revisions' => 'Anzahl der Diskussionsbearbeitungen',
+	'sesp-property-mimetype' => 'MIME-Type',
+	'sesp-property-mediatype' => 'Medientyp',
+	'sesp-property-shorturl' => 'Kurz-URL',
 );
  
 /** Swedish (Svenska)
  * @author Rotsee
  */
 $messages['sv'] = array(
-        'sesp-desc' => 'Extra specialegenskaper på alla sidor',
-        'sesp-property-author' => 'Sidförfattare',
-        'sesp-property-first-author' => 'Sidskapare',
-        'sesp-property-revision-id' => 'Versions-id',
-        'sesp-property-view-count' => 'Antal sidvisningar',
-        'sesp-property-subpages' => 'Undersida',
-        'sesp-property-revisions' => 'Antal sidredigeringar',
-        'sesp-property-talk-revisions' => 'Antal redigeringar på diskussionssidan',
-        'sesp-property-mimetype' => 'MIME-typ',
-        'sesp-property-mediatype' => 'Medietyp',
-        'sesp-property-shorturl' => 'Kortlänk',
+	'sesp-desc' => 'Extra specialegenskaper på alla sidor',
+	'sesp-property-author' => 'Sidförfattare',
+	'sesp-property-first-author' => 'Sidskapare',
+	'sesp-property-revision-id' => 'Versions-id',
+	'sesp-property-view-count' => 'Antal sidvisningar',
+	'sesp-property-subpages' => 'Undersida',
+	'sesp-property-revisions' => 'Antal sidredigeringar',
+	'sesp-property-talk-revisions' => 'Antal redigeringar på diskussionssidan',
+	'sesp-property-mimetype' => 'MIME-typ',
+	'sesp-property-mediatype' => 'Medietyp',
+	'sesp-property-shorturl' => 'Kortlänk',
 	'sesp-property-user-registration-date' => 'Användarens registreringsdatum',
 );
 
