58d99621f91ffa8cfbc32c079172c9afb135d627
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-02-20 21:30:35 +0100

    Update SemanticExtraSpecialProperties.php
    * Improved and updated file documentation
    * Misc tweaks

--- SemanticExtraSpecialProperties.php
+++ SemanticExtraSpecialProperties.php
@@ -2,21 +2,23 @@
 
 /**
  * Extension SemanticExtraSpecialProperties - Adds some extra special properties to all pages.
- * @version 0.2.6 - 2012/10/05
  *
- * @link http://www.mediawiki.org/wiki/Extension:SemanticExtraSpecialProperties Documentation
+ * @link https://github.com/SemanticMediaWiki/SemanticExtraSpecialProperties/blob/master/README.md Documentation
+ * @link https://github.com/SemanticMediaWiki/SemanticExtraSpecialProperties/blob/master/CHANGELOG.md Changlog
+ * @link https://github.com/SemanticMediaWiki/SemanticExtraSpecialProperties/issues Support
+ * @link https://github.com/SemanticMediaWiki/SemanticExtraSpecialProperties Source code
  *
- * @file SemanticExtraSpecialProperties.php
- * @ingroup Extensions
- * @package MediaWiki
- * @author Leo Wallentin (Rotsee), mwjames,  nischayn22
- * @license http://www.opensource.org/licenses/BSD-2-Clause BSD
+ * @author Leo Wallentin (Rotsee)
+ * @author James Hong Kong (Mwjames)
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
+// Prevent direct entry
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
+// Assure compatibility
 if ( version_compare( $GLOBALS['wgVersion'], '1.19', '<' ) ) {
 	die( '<b>Error:</b> This version of Semantic Extra Special Properties requires MediaWiki 1.20 or above.' );
 }
@@ -29,25 +31,28 @@ if ( version_compare( SMW_VERSION, '1.7', '<' ) ) {
 	die( '<b>Error:</b> This version of Semantic Extra Special Properties requires Semantic MediaWiki 1.7 or above.' );
 }
 
+// Define version
 define( 'SESP_VERSION', '0.3 alpha' );
 
+// Register extension
 $GLOBALS['wgExtensionCredits']['semantic'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'Semantic Extra Special Properties',
 	'author'         => array(
 		'[https://github.com/rotsee Leo Wallentin]',
 		'[http://xn--ssongsmat-v2a.nu Säsongsmat.nu]',
-		'[https://semantic-mediawiki.org/wiki/User:MWJames mwjames]'
+		'[https://semantic-mediawiki.org/wiki/User:MWJames James Hong Kong]'
 	),
 	'version'        => SESP_VERSION,
-	'url'            => 'http://www.mediawiki.org/wiki/Extension:SemanticExtraSpecialProperties',
-	'descriptionmsg' => 'sesp-desc',
+	'url'            => 'https://www.mediawiki.org/wiki/Extension:SemanticExtraSpecialProperties',
+	'descriptionmsg' => 'sesp-desc'
 );
 
+// Tell file locations
 $GLOBALS['wgExtensionMessagesFiles']['SemanticESP'] = __DIR__ . '/SemanticExtraSpecialProperties.i18n.php';
 
 $GLOBALS['wgAutoloadClasses']['SESP'] = __DIR__ . '/src/SESP.php';
 
+// Register hooks
 $GLOBALS['wgHooks']['smwInitProperties'][] = 'SESP::sespInitProperties';
 $GLOBALS['wgHooks']['SMWStore::updateDataBefore'][] = 'SESP::sespUpdateDataBefore';
-
