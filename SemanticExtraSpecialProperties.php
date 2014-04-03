a65ca8745cb289c5c2c540c954108e1da6c2f5d6
mwjames <jamesin.hongkong.1@gmail.com>
2014-04-04 02:12:00 +0900

    Update SemanticExtraSpecialProperties.php

--- SemanticExtraSpecialProperties.php
+++ SemanticExtraSpecialProperties.php
@@ -30,7 +30,7 @@ if ( defined( 'SESP_VERSION' ) ) {
 	return 1;
 }
 
-define( 'SESP_VERSION', '1.1-alpha' );
+define( 'SESP_VERSION', '1.1.1' );
 
 if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 	include_once( __DIR__ . '/vendor/autoload.php' );
