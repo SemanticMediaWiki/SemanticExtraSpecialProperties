6374a2347c9bf4dd0e55286326eebc67b0653717
mwjames <jamesin.hongkong.1@gmail.com>
2015-10-04 17:20:42 +0200

    Format phpunit cli output

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,11 +4,11 @@ if ( PHP_SAPI !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
-if ( is_readable( $autoloaderClassPath = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
-	print "\nSemanticMediaWiki " . SMW_VERSION . " ({$GLOBALS['wgDBtype']}) test autoloader ...\n";
-} else {
+if ( !is_readable( $autoloaderClassPath = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
 	die( 'The SemanticMediaWiki test autoloader is not available' );
 }
+
+print sprintf( "\n%-20s%s\n", "Semantic Extra Special Properties: ", SESP_VERSION );
 
 $autoloader = require $autoloaderClassPath;
 $autoloader->addPsr4( 'SESP\\Tests\\', __DIR__ . '/phpunit/Unit' );
