bda9d9bfea159b8377958151d191d70d30a81ed4
Bertrand Gorge <bertrand.gorge@neayi.com>
2022-12-06 16:23:13 +0100

    Added a new property Description from Description2

---
+++ src/PropertyAnnotators/PageDescriptionPropertyAnnotator.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace SESP\PropertyAnnotators;
+
+use SMW\DIProperty;
+use SMW\SemanticData;
+use SMWDIString as DIString;
+use SESP\PropertyAnnotator;
+use SESP\AppFactory;
+use ParserOptions;
+
+/**
+ * @private
+ * @ingroup SESP
+ *
+ * @license GNU GPL v2+
+ * @since 2.0
+ *
+ * @author mwjames
+ */
+class PageDescriptionPropertyAnnotator implements PropertyAnnotator {
+
+	/**
+	 * Predefined property ID
+	 */
+	const PROP_ID = '___DESCRIPTION';
+
+	/**
+	 * @var AppFactory
+	 */
+	private $appFactory;
+
+	/**
+	 * @since 2.0
+	 *
+	 * @param AppFactory $appFactory
+	 */
+	public function __construct( AppFactory $appFactory ) {
+		$this->appFactory = $appFactory;
+	}
+
+	/**
+	 * @since 2.0
+	 *
+	 * {@inheritDoc}
+	 */
+	public function isAnnotatorFor( DIProperty $property ) {
+		return $property->getKey() === self::PROP_ID;
+	}
+
+	/**
+	 * @since 2.0
+	 *
+	 * {@inheritDoc}
+	 */
+	public function addAnnotation( DIProperty $property, SemanticData $semanticData ) {
+
+		$page = $this->appFactory->newWikiPage( $semanticData->getSubject()->getTitle() );
+		$description = $this->getPageViewCount( $page );
+
+		if ( !empty( $description ) ) {
+			$semanticData->addPropertyObjectValue( $property, new DIString( $description ) );
+		}
+	}
+
+	private function getPageViewCount( $page ) {
+
+		$parser =  $page->getParserOutput( ParserOptions::newCanonical( 'canonical' ) );
+		$description = $parser->getProperty( 'description' );
+
+		if ( $description !== false ) { // set by Description2 extension, install it if you want proper og:description support
+			return $description;
+		}
+
+		return null;
+	}
+
+}
