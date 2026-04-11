661b4ecaca837afbb6fb5eefaf507f62ddd45bae
paladox <paladox@users.noreply.github.com>
2026-04-11 17:45:04 +0100

    Update PageImagesPropertyAnnotatorTest.php

--- tests/phpunit/Unit/PropertyAnnotators/PageImagesPropertyAnnotatorTest.php
+++ tests/phpunit/Unit/PropertyAnnotators/PageImagesPropertyAnnotatorTest.php
@@ -2,12 +2,12 @@
 
 namespace SESP\Tests\PropertyAnnotators;
 
+use MediaWiki\Title\Title;
 use SESP\AppFactory;
 use SESP\PropertyAnnotators\PageImagesPropertyAnnotator;
 use SMW\DIProperty;
 use SMW\DIWikiPage;
 use SMW\SemanticData;
-use Title;
 
 /**
  * @covers \SESP\PropertyAnnotators\PageImagesPropertyAnnotator
